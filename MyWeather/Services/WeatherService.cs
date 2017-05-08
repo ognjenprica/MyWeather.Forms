ed3f6884e2d433ea4d0f6674e7b6548e9b426ecf
Ognjen Prica <ognjenp@sigmasoft.ca>
2017-05-08 17:10:08 -0400

    Switched calls based on GPS to use SLG web API server

--- MyWeather/Services/WeatherService.cs
+++ MyWeather/Services/WeatherService.cs
@@ -13,19 +13,22 @@ namespace MyWeather.Services
 
     public class WeatherService
     {
-        const string WeatherCoordinatesUri = "http://api.openweathermap.org/data/2.5/weather?lat={0}&lon={1}&units={2}&appid=fc9f6c524fc093759cd28d41fda89a1b";
+        //const string WeatherCoordinatesUri = "http://api.openweathermap.org/data/2.5/weather?lat={0}&lon={1}&units={2}&appid=fc9f6c524fc093759cd28d41fda89a1b";
+        const string WeatherCoordinatesUri = "http://slg-compass360-azure-api.azurewebsites.net/api/location/weather?latitude={0}&longitude={1}";
 
         //const string WeatherCityUri = "http://api.openweathermap.org/data/2.5/weather?q={0}&units={1}&appid=fc9f6c524fc093759cd28d41fda89a1b";
         const string WeatherCityUri = "http://slg-compass360-azure-api.azurewebsites.net/api/city/weather?city={0}&units={1}";
 
-        const string ForecaseUri = "http://api.openweathermap.org/data/2.5/forecast?id={0}&units={1}&appid=fc9f6c524fc093759cd28d41fda89a1b";
+        //const string ForecaseUri = "http://api.openweathermap.org/data/2.5/forecast?id={0}&units={1}&appid=fc9f6c524fc093759cd28d41fda89a1b";
         const string ForecastByNameUri = "http://slg-compass360-azure-api.azurewebsites.net/api/city/forecast?city={0}&units={1}";
+        const string ForecastUri = "http://slg-compass360-azure-api.azurewebsites.net/api/location/forecast?id={0}&units={1}";
 
-        public async Task<WeatherRoot> GetWeather(double latitude, double longitude, Units units = Units.Imperial)
+        public async Task<WeatherRoot> GetWeather(double latitude, double longitude, Units units = Units.Metric)
         {
             using (var client = new HttpClient())
             {
-                var url = string.Format(WeatherCoordinatesUri, latitude, longitude, units.ToString().ToLower());
+                //var url = string.Format(WeatherCoordinatesUri, latitude, longitude, units);
+                var url = string.Format(WeatherCoordinatesUri, latitude, longitude);
                 var json = await client.GetStringAsync(url);
 
                 if (string.IsNullOrWhiteSpace(json))
@@ -35,6 +38,7 @@ namespace MyWeather.Services
             }
 
         }
+
 
         public async Task<WeatherRoot> GetWeather(string city, Units units = Units.Imperial)
         {
@@ -56,7 +60,8 @@ namespace MyWeather.Services
         {
             using (var client = new HttpClient())
             {
-                var url = string.Format(ForecaseUri, id, units.ToString().ToLower());
+                //var url = string.Format(ForecaseUri, id, units.ToString().ToLower());
+                var url = string.Format(ForecastUri, id, units);
 
                 var json = await client.GetStringAsync(url);
 
